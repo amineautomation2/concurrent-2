7ade05ea153de9af414b45fb80487184c886811a
aminerwx <aminerwx@gmail.com>
2026-05-03 10:21:42 +0100

    deploy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import time
 from ajbell import ajbell_runner
 from ajbell.url import get_ajbell_url
-from utils import delay, get_xlsx_filepath
+from utils import create_spreadsheet, delay, get_xlsx_filepath
 from worker import (
     merge_csv_to_xlsx,
 )
@@ -11,27 +11,24 @@ from worker import (
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", type=str, help="id worker")
+    parser.add_argument("--max", type=str, help="max worker")
     parser.add_argument("--sheet", type=str, help="sheet name")
-    parser.add_argument("--url", type=str, help="sheet name")
+    parser.add_argument("--url", action="store_true", help="sheet name")
 
     args = parser.parse_args()
     xlsx_out = get_xlsx_filepath("ajbell.xlsx")
     if args.url:
+        create_spreadsheet(xlsx_out, ["Investment", "ETF", "MF"], [
+                           "Name", "ISIN", "URL", "Keyword"])
         sheet = args.url
-        if args.url == "all":
-            for sheet in ["Investment", "ETF", "MF"]:
-                get_ajbell_url(sheet)
-                delay(10, 20)
-            return
-        get_ajbell_url(sheet)
+        for sheet in ["Investment", "ETF", "MF"]:
+            get_ajbell_url(sheet)
+            delay(10, 20)
+        return
 
-    elif args.id and args.sheet:
-        # id_worker = int(sys.argv[1])
-        start = time.perf_counter()
+    elif args.id and args.max and args.sheet:
         ajbell_runner(id_worker=int(args.id),
-                      max_workers=5, sheet=args.sheet)
-        elapsed = time.perf_counter() - start
-        print(f"Execution time: {elapsed:.2f} seconds.")
+                      max_workers=int(args.max), sheet=args.sheet)
         return
 
     elif args.sheet:
@@ -41,4 +38,7 @@ def main():
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     main()
+    elapsed = time.perf_counter() - start
+    print(f"Execution time: {elapsed:.2f} seconds.")
